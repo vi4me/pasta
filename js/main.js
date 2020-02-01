28077db1faaeb7a404abbf2617a9c31559b3ff13
vi4me <https://github.com/vi4me/Read.git>
2020-02-01 16:44:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$(function(){
+
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
+// scrolling
+  var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+  function(callback){ window.setTimeout(callback, 1000/60)};
+  var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+  function loop() {
+
+	Array.prototype.forEach.call(elementsToShow, function(element){
+	  if (isElementInViewport(element)) {
+	    element.classList.add('is-visible');
+	  } else {
+	    element.classList.remove('is-visible');
+	  }
+	});
+
+	scroll(loop);
+  }
+
+// Call the loop for the first time
+  loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 100, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 2500, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+
+});
+ScrollOut({
+      targets: 'h2,h3,p,a'
+    })
+});
